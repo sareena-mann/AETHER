6c5a9fac3ea5a86ab9661c3f6675b2ffdcb3b2e0
Sareena <sareenamann@berkeley.edu>
2025-07-11 12:24:10 -0700

    figured out how to use medipipe to get pose estimation, but I want to build the model from scrach

--- camera_input.py
+++ camera_input.py
@@ -1,25 +1,35 @@
+#POV this is a cheat code, mediapipe does everything for us...how hard do i want to make this for myself
+
 import cv2
-import sys
+import mediapipe as mp
 
-#Camera device index
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+#Camera device index --> Video Capture Object
 device = 0
-
-#Checks if there is a command line specification to override device values
-if len(sys.argv) > 1:
-    device = sys.argv[1]
-
-#Video Capture object
 source = cv2.VideoCapture(device)
 
 win_name = 'Laptop Camera Input'
 #WINDOW_NORMAL: resizable by user, change to WINDOW_AUTOSIZE?
 cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 
-while cv2.waitKey(1) != 27: # Escape key
-    has_frame, frame = source.read()
-    if not has_frame:
-        break
-    cv2.imshow(win_name, frame)
+#Instantiate mp Hands model
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
+    while cv2.waitKey(1) != 27: # Escape key
+        has_frame, frame = source.read()
+        if not has_frame:
+            break
+
+        #Detections
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        image.flags.writeable = False
+        results = hands.process(image)
+        image.flags.writeable = True
+        image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+
+
+        cv2.imshow(win_name, frame)
 
 source.release()
 cv2.destroyWindow(win_name)
