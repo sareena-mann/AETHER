0ea378b4a2bea97ac2c34edc89ccb49ee09a9cdc
Sareena <sareenamann@berkeley.edu>
2025-07-11 14:23:38 -0700

    org

--- hand_detection.py
+++
@@ -1,4 +0,0 @@
-# STEP 1: get camera_input / frame and be able to decide if there is a hand present or not
-# STEP 2: Recognize and map the joints of the hand
-# STEP 3: x, y coordinates
-# STEP 4: x, y, z coordinates
