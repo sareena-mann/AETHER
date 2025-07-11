984a6bba862497d18c3b45fb883df4a5605003e2
Sareena <sareenamann@berkeley.edu>
2025-07-11 14:09:27 -0700

    proof of conceptfor hand detection with mediapipe

--- camera_input.py
+++ camera_input.py
@@ -1,4 +1,4 @@
-#POV this is a cheat code, mediapipe does everything for us...how hard do i want to make this for myself
+#Epoch 1: get this to work with Mediapipe then just use np to make our own NN
 
 import cv2
 import mediapipe as mp
@@ -15,7 +15,7 @@ win_name = 'Laptop Camera Input'
 cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 
 #Instantiate mp Hands model
-with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) as hands:
+with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5, max_num_hands=1) as hands:
     while cv2.waitKey(1) != 27: # Escape key
         has_frame, frame = source.read()
         if not has_frame:
@@ -26,10 +26,15 @@ with mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5) a
         image.flags.writeable = False
         results = hands.process(image)
         image.flags.writeable = True
-        image = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
+        #results.multi_hand_landmarks gives x, y, z coordinates array of [axis: value]
+        if results.multi_hand_landmarks:
+            for num, hand in enumerate(results.multi_hand_landmarks):
+                #HAND_CONNECTIONS: [part of hand (i.e. wrist): another part]
+                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS)
 
-        cv2.imshow(win_name, frame)
+        cv2.imshow(win_name, image)
 
 source.release()
 cv2.destroyWindow(win_name)
